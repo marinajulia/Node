71b0ecf08dbbb918d6aae842bc3617ce259c68f6
Marina Julia <marinajulia707@gmail.com>
2021-06-16 20:44:25 -0300

    separando app de server

--- node-str/bin/server.js
+++ node-str/bin/server.js
@@ -1,30 +1,16 @@
-'use strict'
-
-const http = require('http');
+const app = require('../src/app')
 const debug = require('debug')('nodestr:server');
-const express = require('express');
+const http = require('http');//carrega o modulo http no node
 
 
-const app = express();
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
-const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
-});
-
-app.use('/', route);
+const server = http.createServer(app); // cria um servidor http
 
 server.listen(port);
 server.on('error', onError);
 server.on('Listening', onListening);
-
 console.log('API rodando na porta ' + port);
 
 function normalizePort(val) {
