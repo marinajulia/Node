d65017f64642456e95f4cecb885448e86b2efad7
marinajulia <marinajulia707@gmail.com>
2021-10-02 19:25:24 -0300

    product

--- node-str/src/app.js
+++ node-str/src/app.js
@@ -2,10 +2,12 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
 
+mongoose.connect('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false');
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
 
