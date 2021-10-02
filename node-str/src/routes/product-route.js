04c784a80fc9d22e527fcdecbf41e1c24659b820
marinajulia <marinajulia707@gmail.com>
2021-10-02 20:49:53 -0300

    get by id

--- node-str/src/routes/product-route.js
+++ node-str/src/routes/product-route.js
@@ -10,6 +10,8 @@ router.get('/', controller.get);
 
 router.get('/:slug', controller.getBySlug);
 
+router.get('/admin/:id', controller.getById);
+
 router.put('/:id', controller.put);
 
 router.delete('/', controller.delete);
