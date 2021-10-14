4269109dc433a261b79e4b81231152787164177b
marinajulia <marinajulia707@gmail.com>
2021-10-13 23:06:13 -0300

    envio de emails

--- node-str/src/config.js
+++ node-str/src/config.js
@@ -1,8 +1,8 @@
 global.SALT_KEY = 'f5b96262-5604-4cd3-95f2-05e78e5761ef';
-global.EMAIL_TMPL = '<strong>{0}</strong';
+global.EMAIL_TMPL = 'Olá!,<strong>{0}</strong, seja bem vindo a node Store';
 
 module.exports = {
     connectionString: 'mongodb://localhost:27017/Node?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false',
-    sendgridKey: 'TBD',
+    sendgridKey: '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',
     containerConnectionString: 'TBD'
 }
--- node-str/src/controllers/customerController.js
+++ node-str/src/controllers/customerController.js
@@ -4,6 +4,8 @@ const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/customerRepository');
 const md5 = require("md5");
 
+const emailService = require('../services/emailService');
+
 exports.post = async (req, res, next) => {
     var contract = new ValidationContract();
     if (!contract.isValid()) {
@@ -15,7 +17,13 @@ exports.post = async (req, res, next) => {
             name: req.body.name,
             email: req.body.email,
             password: md5(req.body.password + global.SALT_KEY)
-        })
+        });
+
+        emailService.send
+            (req.body.email,
+                'Bem vindo ao node Store',
+                global.EMAIL_TMPL.replace('{0}', req.body.name));
+
         res.status(201).send({ message: 'Produto cadastrado com sucesso' });
     } catch (e) {
         res.status(500).send({
